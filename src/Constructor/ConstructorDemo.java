624d3f338aa48a95c7c264b7a8d5d9b96dea6de1
vishalp2341 <vishalp2341@gmail.com>
2024-06-16 16:11:40 +0530

    na

---
+++ src/Constructor/ConstructorDemo.java
@@ -0,0 +1,59 @@
+package Constructor;
+
+/*a demo of zero parameter constructor and constructor of 3 parameters*/
+
+class Demo
+{
+	private int Cid;
+	private String Cname;
+	private int Cno;
+	
+public Demo() {
+		this.Cid = 1;
+		this.Cname = "vishal";
+		this.Cno = 9875322;
+	}
+	
+
+
+	public Demo(int cid, String cname, int cno) {
+	
+	this.Cid = cid;
+	this.Cname = cname;
+	this.Cno = cno;
+}
+
+
+
+	public int getCid() {
+		return Cid;
+	}
+	public String getCname() {
+		return Cname;
+	}
+	public int getCno() {
+		return Cno;
+	}
+}
+
+
+
+
+public class ConstructorDemo {
+
+	public static void main(String[] args) {
+		
+		Demo d1 = new Demo();
+		System.out.println(d1.getCid());
+		System.out.println(d1.getCname());
+		System.out.println(d1.getCno());
+		
+		
+		Demo d2 = new Demo(2,"yoyo", 17813);
+		System.out.println(d2.getCid());
+		System.out.println(d2.getCname());
+		System.out.println(d2.getCno());
+
+	}
+
+}
