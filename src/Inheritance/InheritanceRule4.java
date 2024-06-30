c6b9c2d669f7b1c28b5a89123be00210631dd88b
vishalp2341 <vishalp2341@gmail.com>
2024-07-01 01:25:46 +0530

    4

---
+++ src/Inheritance/InheritanceRule4.java
@@ -0,0 +1,20 @@
+package Inheritance;
+
+//cyclic inheritance is not permitted
+
+
+//class a1 extends a2{
+//	
+//}
+//
+//class a2 extends a1{
+//	
+//}
+public class InheritanceRule4 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
