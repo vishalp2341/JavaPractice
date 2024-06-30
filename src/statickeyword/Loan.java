d47d9db7078c7b8d92e358d786dca0549eeb9f99
vishalp2341 <vishalp2341@gmail.com>
2024-06-30 21:55:21 +0530

    si using static

--- src/statickeyword/Loan.java
+++ src/statickeyword/Loan.java
@@ -1,28 +1,35 @@
 package statickeyword;
-
+// single copy of the variable r is created and  given to all objects in
+//class using static variable and staic block
 import java.util.Scanner;
 
 class Launcher {
-	float p, t;
-	float r = 15.2f;
+	float p;
+	float t;
+	static float r;
+	float si;
+	
+	static
+	{
+		r = 15.2f;
+	}
 
 	public void acceptIP()
 
 	{
 		Scanner scan = new Scanner(System.in);
-		System.out.println("enter p");
+		System.out.println("enter principal amount :");
 		p = scan.nextInt();
-		System.out.println("enter t");
+		System.out.println("enter tenure :");
 		t = scan.nextInt();
-		System.out.println("r is 15.201");
+		
 	}
 
 	void calculate() {
-		float si = (p * t * r) / 100;
-		display(si);
+		 si = (p * t * r) / 100;
 	}
 
-	void display(float si) {
+	void display() {
 		System.out.println("si is " +si );
 	}
 }
@@ -33,15 +40,18 @@ public class Loan {
 		Launcher l1 = new Launcher();
 		l1.acceptIP();
 		l1.calculate();
+		l1.display();
 		
 		
 		Launcher l2 = new Launcher();
 		l2.acceptIP();
 		l2.calculate();
+		l2.display();
 			
 		Launcher l3 = new Launcher();
 		l3.acceptIP();
 		l3.calculate();
+		l3.display();
 		
 	}
 
